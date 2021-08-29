bd925cb246457f10694af60d84acca755db7f0ba
Takumi <nase49946@gmail.com>
2021-08-29 16:24:42 +0900

    reactとstoreの接続

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { createStore } from './reducks/store/store';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
