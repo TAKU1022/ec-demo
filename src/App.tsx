f60bb6aa2f789cc932f3f919f8ae6d5b708fac09
Takumi <nase49946@gmail.com>
2021-08-29 16:25:29 +0900

    storeの取得と変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { RootState } from './reducks/store/store';
+import { signInAction } from './reducks/users/actions';
+
 function App() {
-  return <div className="App"></div>;
+  const dispatch = useDispatch();
+  const selecter = useSelector((state: RootState) => state);
+
+  console.log(selecter.users);
+
+  return (
+    <>
+      <button
+        onClick={() =>
+          dispatch(signInAction({ uid: '00001', username: 'TAKU' }))
+        }
+      >
+        Sign In
+      </button>
+    </>
+  );
 }
 
 export default App;
