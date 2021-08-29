8aaa9bd4d4b9d6f13f7aeb6caeeaedb845db0bf5
Takumi <nase49946@gmail.com>
2021-08-29 13:38:44 +0900

    firebase初期設定追加

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCO_6occyZINlYxdu0RuQ-0Sx5vgvA2I3o",
+  authDomain: "ec-demo-1c4c7.firebaseapp.com",
+  projectId: "ec-demo-1c4c7",
+  storageBucket: "ec-demo-1c4c7.appspot.com",
+  messagingSenderId: "813485818566",
+  appId: "1:813485818566:web:9096deba2e5688a6004dc7",
+  measurementId: "G-0NZCYY4JR7",
+};
