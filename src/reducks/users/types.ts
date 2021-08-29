ea00afdfb7a4cb4e63a1f6386c8d4b7f9ab4db6b
Takumi <nase49946@gmail.com>
2021-08-29 22:56:01 +0900

    型追加

--- src/reducks/users/types.ts
+++ src/reducks/users/types.ts
@@ -1,6 +1,21 @@
+import { Action } from 'redux';
+import { SIGN_IN, SIGN_OUT } from './actionTypes';
+
 export type UserState = {
   icon: string;
   isSignedIn: boolean;
   uid: string;
   username: string;
 };
+
+export interface SignInAction extends Action {
+  type: typeof SIGN_IN;
+  payload: Omit<UserState, 'icon'>;
+}
+
+export interface SignOutAction extends Action {
+  type: typeof SIGN_OUT;
+  payload: Omit<UserState, 'icon'>;
+}
+
+export type UserActionTypes = SignInAction | SignOutAction;
