5b9297be7482e9ac0c003f5349b1cf31febde45a
Takumi <nase49946@gmail.com>
2021-08-29 17:49:55 +0900

    router追加

--- src/reducks/store/store.ts
+++ src/reducks/store/store.ts
@@ -1,15 +1,30 @@
-import { createStore as reduxCreateStore, combineReducers } from 'redux';
+import {
+  connectRouter,
+  routerMiddleware,
+  RouterState,
+} from 'connected-react-router';
+import { History } from 'history';
+import { Reducer } from 'react';
+import {
+  createStore as reduxCreateStore,
+  combineReducers,
+  applyMiddleware,
+  AnyAction,
+} from 'redux';
 import { UsersReducer } from '../users/reducers';
 import { UserState } from '../users/types';
 
 export type RootState = {
+  router: Reducer<RouterState, AnyAction>;
   users: UserState;
 };
 
-export const createStore = () => {
+export const createStore = (history: History) => {
   return reduxCreateStore(
     combineReducers({
+      router: connectRouter(history),
       users: UsersReducer,
-    })
+    }),
+    applyMiddleware(routerMiddleware(history))
   );
 };
