0df5aeafd5a2aabd9661238f64fc8759e00f283e
Takumi <nase49946@gmail.com>
2021-08-31 19:38:13 +0900

    アカウント登録関数追加

--- src/reducks/users/operations.ts
+++ src/reducks/users/operations.ts
@@ -1,5 +1,6 @@
 import { push } from 'connected-react-router';
 import { Dispatch } from 'react';
+import { auth, db, FirebaseTimestamp } from '../../firebase';
 import { signInAction } from './actions';
 
 export const signIn = () => {
@@ -22,5 +23,56 @@ export const signIn = () => {
       );
       dispatch(push('/'));
     }
+  };
+};
+
+export const signUp = (
+  username: string,
+  email: string,
+  pasword: string,
+  confirmPasword: string
+) => {
+  return async (dispach: Dispatch<any>) => {
+    if (
+      username === '' ||
+      email === '' ||
+      pasword === '' ||
+      confirmPasword === ''
+    ) {
+      alert('必須項目が未入力です');
+      return;
+    }
+
+    if (pasword !== confirmPasword) {
+      alert('パスワードが一致しません。もう１度お試しください。');
+      return;
+    }
+
+    return auth
+      .createUserWithEmailAndPassword(email, pasword)
+      .then((result) => {
+        const user = result.user;
+
+        if (user) {
+          const uid = user.uid;
+          const timestamp = FirebaseTimestamp.now();
+
+          const userInitialData = {
+            createdAt: timestamp,
+            email,
+            role: 'customer',
+            uid,
+            updatedAt: timestamp,
+            username,
+          };
+
+          db.collection('users')
+            .doc(uid)
+            .set(userInitialData)
+            .then(() => {
+              dispach(push('/'));
+            });
+        }
+      });
   };
 };
