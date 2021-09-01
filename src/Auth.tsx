4657621aafa39a25be3c7c8f1343213d1c2dea7d
Takumi <nase49946@gmail.com>
2021-09-01 23:32:34 +0900

    認証確認コンポーネント作成

---
+++ src/Auth.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, useEffect, VFC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './reducks/store/store';
+import { listenAuthState } from './reducks/users/operations';
+import { getIsSignedIn } from './reducks/users/selecter';
+
+type Props = {
+  children: ReactNode;
+};
+
+const Auth: VFC<Props> = (props: Props) => {
+  const { children } = props;
+  const dispatch = useDispatch();
+  const selecter = useSelector((state: RootState) => state);
+  const isSginedIn = getIsSignedIn(selecter);
+
+  useEffect(() => {
+    if (!isSginedIn) {
+      dispatch(listenAuthState());
+    }
+  }, []);
+
+  if (!isSginedIn) {
+    return <></>;
+  } else {
+    return <>{children}</>;
+  }
+};
+
+export default Auth;
