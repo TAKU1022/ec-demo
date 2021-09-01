a8fed370b7e08836c406799390bfcd17cec5d142
Takumi <nase49946@gmail.com>
2021-09-01 23:32:06 +0900

    認証情報セレクタ作成

--- src/reducks/users/selecter.ts
+++ src/reducks/users/selecter.ts
@@ -3,6 +3,11 @@ import { RootState } from '../store/store';
 
 const usersSelecter = (state: RootState) => state.users;
 
+export const getIsSignedIn = createSelector(
+  [usersSelecter],
+  (state) => state.isSignedIn
+);
+
 export const getUserId = createSelector([usersSelecter], (state) => state.uid);
 
 export const getUsername = createSelector(
