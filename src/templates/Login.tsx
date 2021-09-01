c3aab1cdd8dd7a19c27fef9fd2b3fa7cf833d592
Takumi <nase49946@gmail.com>
2021-09-01 23:45:01 +0900

    不要コード削除

--- src/templates/Login.tsx
+++ src/templates/Login.tsx
@@ -1,22 +1,7 @@
 import { VFC } from 'react';
-import { useDispatch } from 'react-redux';
-import { signIn } from '../reducks/users/operations';
 
 const Login: VFC = () => {
-  const dispatch = useDispatch();
-
-  return (
-    <div>
-      {/* <h2>ログイン</h2>
-      <button
-        onClick={() => {
-          dispatch(signIn());
-        }}
-      >
-        ログインする
-      </button> */}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Login;
