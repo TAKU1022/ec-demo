929bfabc15ec8c8ed24b51d4a903fe9c8a7ddad0
Takumi <nase49946@gmail.com>
2021-09-04 16:12:32 +0900

    商品登録機能実装

--- src/templates/ProductEdit.tsx
+++ src/templates/ProductEdit.tsx
@@ -1,7 +1,11 @@
 import { ChangeEvent, useCallback, useState, VFC } from 'react';
-import { SelectBox, TextInput } from '../components/UIkit';
+import { useDispatch } from 'react-redux';
+import { PrimaryButton, SelectBox, TextInput } from '../components/UIkit';
+import { saveProduct } from '../reducks/products/operations';
 
 const ProductEdit: VFC = () => {
+  const dispatch = useDispatch();
+
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [category, setCategory] = useState('');
@@ -95,6 +99,15 @@ const ProductEdit: VFC = () => {
           type={'number'}
           onChange={inputPrice}
         />
+        <div className="module-spacer--medium"></div>
+        <div className="center">
+          <PrimaryButton
+            label={'商品情報を保存'}
+            onClick={() =>
+              dispatch(saveProduct(name, description, category, gender, price))
+            }
+          />
+        </div>
       </div>
     </section>
   );
