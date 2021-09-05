5b3b844da7375ebef225f9418c9d6f4f109d64b7
Takumi <nase49946@gmail.com>
2021-09-05 22:44:37 +0900

    商品カードコンポーネントのスタイルとデータの受け渡し実装

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,9 +1,78 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import { push } from 'connected-react-router';
 import { VFC } from 'react';
+import { useDispatch } from 'react-redux';
+import NoImage from '../../assets/img/src/no_image.png';
 
-type Props = {};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down('sm')]: {
+      margin: 8,
+      width: 'calc(50% - 16px)',
+    },
+    [theme.breakpoints.up('sm')]: {
+      margin: 16,
+      width: 'calc(33.3333% - 32px)',
+    },
+  },
+  content: {
+    display: 'flex',
+    padding: '16px 8px',
+    textAlign: 'left',
+    '&:last-child': {
+      paddingBottom: 16,
+    },
+  },
+  media: {
+    height: 0,
+    paddingBottom: '100%',
+  },
+  price: {
+    color: theme.palette.secondary.main,
+    fontSize: 16,
+  },
+}));
 
-const ProductCard: VFC = (props: Props) => {
-  return <div>商品情報</div>;
+type Props = {
+  id: string;
+  images: Array<{ id: string; path: string }>;
+  name: string;
+  price: number;
+};
+
+const ProductCard: VFC<Props> = (props: Props) => {
+  const { id, images, name, price } = props;
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  const productImage = images.length > 0 ? images : [{ path: NoImage }];
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={productImage[0].path}
+        title=""
+        onClick={() => dispatch(push(`/product/${id}`))}
+      />
+      <CardContent className={classes.content}>
+        <div onClick={() => dispatch(push(`/product/${id}`))}>
+          <Typography color="textSecondary" component="p">
+            {name}
+          </Typography>
+          <Typography className={classes.price} component="p">
+            ¥{price.toLocaleString()}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ProductCard;
--- src/templates/ProductList.tsx
+++ src/templates/ProductList.tsx
@@ -1,9 +1,33 @@
-import { VFC } from 'react';
+import { useEffect, VFC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ProductCard } from '../components/products';
+import { fetchProducts } from '../reducks/products/operations';
+import { getProductList } from '../reducks/products/selectors';
+import { RootState } from '../reducks/store/store';
 
 const ProductList: VFC = () => {
+  const dispatch = useDispatch();
+  const selector = useSelector((state: RootState) => state);
+  const products = getProductList(selector);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []); // eslint-disable-line
+
   return (
     <section className="c-section-wrapin">
-      <div className="p-grid__row"></div>
+      <div className="p-grid__row">
+        {products.length > 0 &&
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              images={product.images}
+              name={product.name}
+              price={product.price}
+            />
+          ))}
+      </div>
     </section>
   );
 };
