fad8df758b352c0bf551c8cbca9a31ca1fd1f5ee
Takumi <nase49946@gmail.com>
2021-09-11 21:44:46 +0900

    商品注文機能実装

--- src/reducks/products/operations.ts
+++ src/reducks/products/operations.ts
@@ -5,6 +5,7 @@ import { deleteProductAction, fetchProductsAction } from './actions';
 import { RootState } from '../store/store';
 import { ThunkDispatch } from 'redux-thunk';
 import { Action } from 'redux';
+import { ProductInCart } from '../../types/Cart';
 
 const productRef = db.collection('products');
 
@@ -91,5 +92,98 @@ export const deleteProduct = (id: string) => {
 
         dispatch(deleteProductAction(nextProducts));
       });
+  };
+};
+
+export const orderProduct = (
+  productsInCart: Array<ProductInCart>,
+  amount: number
+) => {
+  return async (
+    dispatch: ThunkDispatch<RootState, unknown, Action>,
+    getState: () => RootState
+  ) => {
+    const uid = getState().users.uid;
+    const userRef = db.collection('users').doc(uid);
+    const timestamp = FirebaseTimestamp.now();
+
+    let products: any = {};
+    let soldOutProducts: Array<string> = [];
+
+    const batch = db.batch();
+
+    productsInCart.forEach(async (product) => {
+      const snapshot = await productRef.doc(product.productId).get();
+      const sizes: Array<{ size: string; quantity: number }> =
+        snapshot.data()?.sizes;
+
+      const updatedSizes = sizes.map((size) => {
+        if (size.size === product.size) {
+          if (size.quantity === 0) {
+            soldOutProducts.push(product.name as string);
+            return size;
+          }
+
+          return {
+            size: size.size,
+            quantity: size.quantity - 1,
+          };
+        } else {
+          return size;
+        }
+      });
+
+      products[product.productId as string] = {
+        id: product.productId,
+        images: product.images,
+        name: product.name,
+        price: product.price,
+        size: product.size,
+      };
+
+      batch.update(productRef.doc(product.productId), { sizes: updatedSizes });
+
+      batch.delete(userRef.collection('cart').doc(product.cartId));
+    });
+
+    if (soldOutProducts.length > 0) {
+      const errorMessage =
+        soldOutProducts.length > 1
+          ? soldOutProducts.join('と')
+          : soldOutProducts[0];
+      alert(
+        `大変申し訳ございません。${errorMessage}が在庫切れとなったため、注文処理を中断しました。`
+      );
+      return;
+    } else {
+      batch
+        .commit()
+        .then(() => {
+          const orderRef = userRef.collection('orders').doc();
+          const date = timestamp.toDate();
+          const shippingDate = FirebaseTimestamp.fromDate(
+            new Date(date.setDate(date.getDate() + 3))
+          );
+
+          const history = {
+            amount,
+            createdAt: timestamp,
+            id: orderRef.id,
+            products,
+            shippingDate,
+            updatedUp: timestamp,
+          };
+
+          orderRef.set(history);
+
+          dispatch(push('/order/complete'));
+        })
+        .catch(() => {
+          alert(
+            '注文処理に失敗しました。通信環境をご確認のうえ、もう一度お試しください。'
+          );
+          return;
+        });
+    }
   };
 };
