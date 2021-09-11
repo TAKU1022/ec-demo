4b0f66c2a8e485e31912bf736fd618fb7925e782
Takumi <nase49946@gmail.com>
2021-09-11 14:39:40 +0900

    カート情報追加

--- src/components/header/HeaderMenus.tsx
+++ src/components/header/HeaderMenus.tsx
@@ -1,6 +1,12 @@
-import { memo, MouseEvent, VFC } from 'react';
+import { memo, MouseEvent, useEffect, VFC } from 'react';
 import { Badge, IconButton } from '@material-ui/core';
 import { FavoriteBorder, Menu, ShoppingCart } from '@material-ui/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../reducks/store/store';
+import { getProductsInCart, getUserId } from '../../reducks/users/selectors';
+import { db } from '../../firebase';
+import { ProductsInCart } from '../../types/Cart';
+import { fetchProductsInCart } from '../../reducks/users/operations';
 
 type Props = {
   handleDrawerToggle: (
@@ -10,11 +16,53 @@ type Props = {
 
 const HeaderMenus: VFC<Props> = memo((props: Props) => {
   const { handleDrawerToggle } = props;
+  const dispatch = useDispatch();
+  const selector = useSelector((state: RootState) => state);
+  const uid = getUserId(selector);
+
+  let productsInCart = getProductsInCart(selector);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('users')
+      .doc(uid)
+      .collection('cart')
+      .onSnapshot((snapshots) => {
+        snapshots.docChanges().forEach((change) => {
+          const productData = change.doc.data();
+          const changeType = change.type;
+
+          switch (changeType) {
+            case 'added':
+              productsInCart.push(productData as ProductsInCart);
+              break;
+            case 'modified':
+              const index = productsInCart.findIndex(
+                (product) => product.cartId === change.doc.id
+              );
+              productsInCart[index] = productData as ProductsInCart;
+              break;
+            case 'removed':
+              // eslint-disable-next-line react-hooks/exhaustive-deps
+              productsInCart = productsInCart.filter(
+                (product) => product.cartId !== change.doc.id
+              );
+              break;
+            default:
+              break;
+          }
+        });
+
+        dispatch(fetchProductsInCart(productsInCart));
+      });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
       <IconButton>
-        <Badge badgeContent={3} color="secondary">
+        <Badge badgeContent={productsInCart.length} color="secondary">
           <ShoppingCart />
         </Badge>
       </IconButton>
