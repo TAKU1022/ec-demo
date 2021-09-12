768634d128c4095a54c81ba73f1e52f95550d0ae
Takumi <nase49946@gmail.com>
2021-09-12 16:56:29 +0900

    型の追加とそれに伴う修正

--- src/reducks/users/types.ts
+++ src/reducks/users/types.ts
@@ -1,6 +1,12 @@
 import { Action } from 'redux';
 import { ProductInCart } from '../../types/Cart';
-import { FETCH_PRODUCTS_IN_CART, SIGN_IN, SIGN_OUT } from './actionTypes';
+import { OrdersHistory } from '../../types/Order';
+import {
+  FETCH_ORDERS_HISTORY,
+  FETCH_PRODUCTS_IN_CART,
+  SIGN_IN,
+  SIGN_OUT,
+} from './actionTypes';
 
 export type UserState = {
   isSignedIn: boolean;
@@ -8,16 +14,17 @@ export type UserState = {
   uid: string;
   username: string;
   cart: Array<ProductInCart>;
+  orders: Array<OrdersHistory>;
 };
 
 export interface SignInAction extends Action {
   type: typeof SIGN_IN;
-  payload: Omit<UserState, 'cart'>;
+  payload: Omit<UserState, 'cart' | 'orders'>;
 }
 
 export interface SignOutAction extends Action {
   type: typeof SIGN_OUT;
-  payload: Omit<UserState, 'cart'>;
+  payload: Omit<UserState, 'cart' | 'orders'>;
 }
 
 export interface FetchProductsInCartAction extends Action {
@@ -25,7 +32,13 @@ export interface FetchProductsInCartAction extends Action {
   payload: Array<ProductInCart>;
 }
 
+export interface FetchOrdersHistoryAction extends Action {
+  type: typeof FETCH_ORDERS_HISTORY;
+  payload: Array<OrdersHistory>;
+}
+
 export type UserActionTypes =
   | SignInAction
   | SignOutAction
-  | FetchProductsInCartAction;
+  | FetchProductsInCartAction
+  | FetchOrdersHistoryAction;
