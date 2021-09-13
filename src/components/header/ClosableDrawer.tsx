c95a5cc77a14ada09f4f0084a83608f6a73d15ef
Takumi <nase49946@gmail.com>
2021-09-13 23:04:53 +0900

    ドロワーカテゴリー表示追加

--- src/components/header/ClosableDrawer.tsx
+++ src/components/header/ClosableDrawer.tsx
@@ -17,8 +17,16 @@ import {
 } from '@material-ui/icons';
 import { createStyles, makeStyles } from '@material-ui/styles';
 import { push } from 'connected-react-router';
-import { ChangeEvent, MouseEvent, useCallback, useState, VFC } from 'react';
+import {
+  ChangeEvent,
+  MouseEvent,
+  useCallback,
+  useEffect,
+  useState,
+  VFC,
+} from 'react';
 import { useDispatch } from 'react-redux';
+import { db } from '../../firebase';
 import { signOut } from '../../reducks/users/operations';
 import { TextInput } from '../UIkit';
 
@@ -70,6 +78,17 @@ const ClosableDrawer: VFC<Props> = (props: Props) => {
     onClose(event);
   };
 
+  const [filters, setFilters] = useState([
+    { func: selectMenu, label: 'すべて', id: 'all', value: '/' },
+    { func: selectMenu, label: 'メンズ', id: 'male', value: '/?gender=male' },
+    {
+      func: selectMenu,
+      label: 'レディース',
+      id: 'female',
+      value: '/?gender=female',
+    },
+  ]);
+
   const menus = [
     {
       func: selectMenu,
@@ -93,6 +112,25 @@ const ClosableDrawer: VFC<Props> = (props: Props) => {
       value: '/user/mypage',
     },
   ];
+
+  useEffect(() => {
+    db.collection('categories')
+      .get()
+      .then((snapshots) => {
+        const list: any[] = [];
+        snapshots.forEach((snapshot) => {
+          const category = snapshot.data();
+          list.push({
+            func: selectMenu,
+            label: category.name,
+            id: category.id,
+            value: `/?category=${category.id}`,
+          });
+        });
+        setFilters((prevState) => [...prevState, ...list]);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <nav className={classes.drawer}>
@@ -139,6 +177,18 @@ const ClosableDrawer: VFC<Props> = (props: Props) => {
               </ListItemIcon>
               <ListItemText primary="logout" />
             </ListItem>
+          </List>
+          <Divider />
+          <List>
+            {filters.map((filter) => (
+              <ListItem
+                button
+                key={filter.id}
+                onClick={(event) => filter.func(filter.value, event)}
+              >
+                <ListItemText primary={filter.label} />
+              </ListItem>
+            ))}
           </List>
         </div>
       </Drawer>
